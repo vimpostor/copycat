42a6684f711432b7096f519976ab57bb61655fa0
Magnus Groß <magnus.gross@rwth-aachen.de>
2022-10-14 18:50:32 +0200

    Add test for openat
    Our original test for open() should actually not work, as we have not
    implemented interception for open(2) yet.
    
    It only works because the glibc interface for open() pretends to be
    open(), but actually calls openat() behind the scenes.
    
    Maybe we should in the future call the syscalls directly, the glibc
    interface adds too much stuff on top of it.

--- tests/tests_general.c
+++ tests/tests_general.c
@@ -1,19 +1,28 @@
+#define _GNU_SOURCE
+
 #include <assert.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void check_correct_fd(const int fd) {
+void check_correct_fd(int fd) {
 	static char c;
 	// we should be able to read from the file descriptor
 	assert(read(fd, &c, 1));
 	// the read char should be 'b', and not 'a'
 	assert(c != 'a');
 	assert(c == 'b');
+
+	// close the descriptor again
+	assert(!close(fd));
 }
 
 int do_open(const char *filename) {
 	return open(filename, O_RDONLY);
+}
+
+int do_openat(const char *filename) {
+	return openat(0, filename, O_RDONLY);
 }
 
 int main(int argc, char *argv[])
@@ -23,6 +32,10 @@ int main(int argc, char *argv[])
 
 	// open()
 	f = do_open(filename);
+	check_correct_fd(f);
+
+	// openat()
+	f = do_openat(filename);
 	check_correct_fd(f);
 
 	printf("All tests passed!\n");
