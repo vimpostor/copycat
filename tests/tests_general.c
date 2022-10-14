e1e042debe3d4239beebf5b35b42ea8febc779fa
Magnus Groß <magnus.gross@rwth-aachen.de>
2022-10-14 18:41:34 +0200

    Generalize test interface

--- tests/tests_general.c
+++ tests/tests_general.c
@@ -3,13 +3,28 @@
 #include <stdio.h>
 #include <unistd.h>
 
+void check_correct_fd(const int fd) {
+	static char c;
+	// we should be able to read from the file descriptor
+	assert(read(fd, &c, 1));
+	// the read char should be 'b', and not 'a'
+	assert(c != 'a');
+	assert(c == 'b');
+}
+
+int do_open(const char *filename) {
+	return open(filename, O_RDONLY);
+}
+
 int main(int argc, char *argv[])
 {
 	const char filename[] = "/tmp/a";
-	char c;
+	int f = -1;
 
-	int f = open(filename, O_RDONLY);
-	assert(read(f, &c, 1));
-	assert(c == 'b');
+	// open()
+	f = do_open(filename);
+	check_correct_fd(f);
+
+	printf("All tests passed!\n");
 	return 0;
 }
