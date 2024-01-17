49e426b19590959583168618fe2c7f6a0396e157
Magnus Groß <magnus@mggross.com>
2024-01-17 23:25:49 +0100

    Add strace comparison

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -13,3 +13,7 @@ echo -e "\nRunning benchmark without interception:"
 ./benchmark
 echo -e "\nRunning benchmark with interception:"
 COPYCAT="/tmp/a /tmp/b" build/copycat -- ./benchmark
+echo -e "\nRunning benchmark with strace:"
+strace --quiet=all -e openat -- ./benchmark 2>/dev/null
+echo -e "\nRunning benchmark with strace --seccomp-bpf:"
+strace -f --seccomp-bpf --quiet=all -e openat -- ./benchmark 2>/dev/null
